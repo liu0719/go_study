03e9353133f00449726592e76289a4ef5012aeed
liu <3354293667@qq.com>
2026-07-16 16:32:49 +0800

    🌐 自定义类型名和别名

--- 8.自定义类型和别名/自定义类型名和别名.go
+++ 8.自定义类型和别名/自定义类型名和别名.go
@@ -4,7 +4,7 @@ import "fmt"
 
 // 自定义类型名和别名
 
-// 类型别名和原类型区别
+// 类型别名和原类型区别:
 // 原类型不能绑定方法
 // 打印类型还是原始类型
 
@@ -15,8 +15,9 @@ const (
 	SUCCESSCODE Code = 0
 	SERVICEERR  Code = 101 //验证失败响应码
 	NETWORKERR  Code = 201 //错误响应码
-	
+
 )
+
 // 根据code的不同类型返回不同结果
 func (c Code) getMsg() string {
 	switch c {
@@ -30,9 +31,10 @@ func (c Code) getMsg() string {
 	}
 	return "未知错误"
 }
+
 // 直接返回两个值
-func (c Code)ok()(Code,string){
-	return c,c.getMsg()
+func (c Code) ok() (Code, string) {
+	return c, c.getMsg()
 }
 func webserve(name string) (code Code, msg string) {
 	if name == "1" {
@@ -45,7 +47,7 @@ func webserve(name string) (code Code, msg string) {
 }
 
 func main() {
-	const age  =1
-	var c Code=1
-	fmt.Println(c==age)
+	const age = 1
+	var c Code = 1
+	fmt.Println(c == age)
 }
